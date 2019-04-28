3d5a46c832301b16064f1e60c0d7aacf8a8f89e1
monte0 <47285725+monte0@users.noreply.github.com>
2019-04-27 21:23:18 -0700

    repair signs

---
+++ Assets/Scripts/fixing.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fixing : MonoBehaviour
+{
+   // public TextAlignment repairing;
+
+    public Image repairButton;
+
+    void Start()
+    {
+        repairButton.gameObject.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("repair"))
+        {
+            repairButton.gameObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        repairButton.gameObject.SetActive(false);
+
+    }
+}
