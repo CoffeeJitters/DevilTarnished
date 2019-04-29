a52855058af29e4b5fb00f8525b4115112467c35
monte0 <47285725+monte0@users.noreply.github.com>
2019-04-28 22:01:43 -0700

    repair bar fixed

--- Assets/Scripts/fixing.cs
+++ Assets/Scripts/fixing.cs
@@ -15,7 +15,9 @@ public class fixing : MonoBehaviour
     public Text repairText3;
     public Image repairBar3;
     public Image filling3;
-    private float repair;
+    private float repair1;
+    private float repair2;
+    private float repair3;
     private float maxrepair;
     bool inEvent1;
     bool inEvent2;
@@ -23,7 +25,9 @@ public class fixing : MonoBehaviour
 
     void Start()
     {
-        repair = 0f;
+        repair1 = 0f;
+        repair2 = 0f;
+        repair3 = 0f;
         maxrepair = 100f;
         filling1.gameObject.SetActive(false);
         repairText1.gameObject.SetActive(false);
@@ -39,42 +43,54 @@ public class fixing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire1") && inEvent1 == true && repair < 101)
+        if (Input.GetButton("Fire1") && inEvent1 == true && repair1 < 101)
         {
-            filling1.fillAmount = repair / maxrepair;
-            repair += 1;
+            filling1.fillAmount = repair1 / maxrepair;
+            repair1 += 1;
+            if(repair1 == 100)
+            {
+                repairText1.text = "Fixed!!";
+            }
         }
         if (inEvent1 == true && !(Input.GetButton("Fire1")))
         {
-            if (repair < 100)
+            if (repair1 < 100)
             {
-                repair = 0;
+                repair1 = 0;
                 filling1.fillAmount = 0;
             }
         }
-        if (Input.GetButton("Fire1") && inEvent2 == true && repair < 101)
+        if (Input.GetButton("Fire1") && inEvent2 == true && repair2 < 101)
         {
-            filling2.fillAmount = repair / maxrepair;
-            repair += 1;
+            filling2.fillAmount = repair2 / maxrepair;
+            repair2 += 1;
+            if (repair2 == 100)
+            {
+                repairText2.text = "Fixed!!";
+            }
         }
         if (inEvent2 == true && !(Input.GetButton("Fire1")))
         {
-            if (repair < 100)
+            if (repair2 < 100)
             {
-                repair = 0;
+                repair2 = 0;
                 filling2.fillAmount = 0;
             }
         }
-        if (Input.GetButton("Fire1") && inEvent == true && repair < 101)
+        if (Input.GetButton("Fire1") && inEvent3 == true && repair3 < 101)
         {
-            filling3.fillAmount = repair / maxrepair;
-            repair += 1;
-        }
-        if (inEvent == true && !(Input.GetButton("Fire1")))
-        {
-            if (repair < 100)
+            filling3.fillAmount = repair3 / maxrepair;
+            repair3 += 1;
+            if (repair3 == 100)
             {
-                repair = 0;
+                repairText3.text = "Fixed!!";
+            }
+        }
+        if (inEvent3 == true && !(Input.GetButton("Fire1")))
+        {
+            if (repair3 < 100)
+            {
+                repair3 = 0;
                 filling3.fillAmount = 0;
             }
         }
@@ -84,21 +100,21 @@ public class fixing : MonoBehaviour
     {
         if (other.gameObject.tag == "repair1")
         {
-            inEvent = true;
+            inEvent1 = true;
             repairText1.gameObject.SetActive(true);
             repairBar1.gameObject.SetActive(true);
             filling1.gameObject.SetActive(true);
         }
         if (other.gameObject.tag == "repair2")
         {
-            inEvent = true;
+            inEvent2 = true;
             repairText2.gameObject.SetActive(true);
             repairBar2.gameObject.SetActive(true);
             filling2.gameObject.SetActive(true);
         }
         if (other.gameObject.tag == "repair3")
         {
-            inEvent = true;
+            inEvent3 = true;
             repairText3.gameObject.SetActive(true);
             repairBar3.gameObject.SetActive(true);
             filling3.gameObject.SetActive(true);
@@ -107,20 +123,14 @@ public class fixing : MonoBehaviour
 
     void OnTriggerExit(Collider other)
     {
-        inEvent = false;
+        inEvent1 = false;
+        inEvent2 = false;
+        inEvent3 = false;
         repairText1.gameObject.SetActive(false);
         repairBar1.gameObject.SetActive(false);
         repairText2.gameObject.SetActive(false);
         repairBar2.gameObject.SetActive(false);
         repairText3.gameObject.SetActive(false);
         repairBar3.gameObject.SetActive(false);
-    }
-
-    void reset()
-    {
-        if (repair != 101)
-        {
-            repair = 0;
-        }
     }
 }
